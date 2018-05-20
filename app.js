0049a5bfeab80d52c210e87ce0f59ff35cf12d8c
ucipass <ucipass@gmail.com>
2018-05-20 08:02:49 -0500

    redeisgn

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var path	= require("path")
 var app     = require('./bin/www.js').app		// Express.js
 var auth 	= require('./bin/auth.js');			// Authentication middleware using Passport (using "app")
 var users 	= require('./bin/users.js').router;	// Router for User Management
-var gallery = require("./routes/gallery/gallery.js")
+
 
 
 
@@ -18,9 +18,8 @@ app.post('/login' ,auth.login);		//Just redirects to login page or original URL 
 app.post('/logout',auth.logout);	//just redirects to login page
 app.get( '/login', root);
 app.get( '/', auth.alreadyLoggedIn,root);
-
 app.use( "/users",auth.alreadyLoggedIn,users)
-app.use( "/gallery",auth.alreadyLoggedIn,gallery)
+
 
 //
 // CATCH ALL BAD ONE REQUEST
